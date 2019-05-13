86536ba035e74829bd8ba3a384bd15e9420219cf
Larry Adigun <larryadigun.student@careerdevs.com>
2019-05-13 15:01:13 +0000

    first commit

--- char.c
+++ char.c
@@ -28,5 +28,19 @@ int main (void)
 
 
     }
+    int main (void)
+{
+    string name = "Larry";
+
+    for( int i = 0, length = strlen(name); i < length ; i++) {
+        //printf("%c\n", name[i]);
+        //printf("letter: \t%i length: \t%i\n", i, length);
+       // printf("%c\n", name[letter]);
+        printf("%c", name[letter]);
+
+
+    }
+
+}
 
 }
